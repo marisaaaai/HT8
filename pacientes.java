bee5e7c5f5786d4eeff40a33407098ec9ed4be30
marisaamv <mon19169@uvg.edu.gt>
2020-04-05 12:34:47 -0600

    Cambios

--- pacientes.java
+++ pacientes.java
@@ -2,11 +2,13 @@
 Maria Isabel Montoya 19169
 Hoja de Trabajo No. 8
 pacientes*/
+import java.util.ArrayList; 
+import java.util.*;
 public class pacientes{
-	private ArrayList info;
+    private ArrayList info;
 	
-	public pacientes(ArrayList<String> patients){
-		this.info = patients;
-	}
+    public pacientes(ArrayList<String> patients){
+	this.info = patients;
+    }
 
 }
