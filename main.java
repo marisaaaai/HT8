bf2b3c19c5fcf62993188c8b6efe07d1f19b3bf6
marisaamv <mon19169@uvg.edu.gt>
2020-04-05 11:30:33 -0600

    Implementacion con JCF

--- main.java
+++ main.java
@@ -8,33 +8,43 @@ import java.util.ArrayList;
 import java.util.*; 
 import java.util.Collection;
 public class main{
-	public static void main(String[]args){
-		ArrayList<String> dataArrayList = new ArrayList<String>;
-		Scanner s = null;
-		String archive = "pacientes.txt";
-		File fichero = new Fichero(archive);
-		String linea="";
-		try{
-			s = Scanner(fichero);
-			while(s.hasNextLine()){
-				linea = s.nextLine();
-			    data [] = linea.split(",");
-				String infoPatient = data[2]+","+ data[1]+","+data[0];
-				dataArrayList.add(infoPatient);
-			}
-		}
-		catch(Exception ex){
-			System.out.println("Se tuvo el siguiente error: "+ex.getMessage());
-		}
-		finally{
-			try{
-				if(s!=null){
-					s.close();
-				}
-			}
-			catch(Exception ex2){
-				System.out.println("Se tuvo el error: "+ ex2.getMessage());
-			}
-		}
+    public static void main(String[]args){
+	ArrayList<String> dataArrayList = new ArrayList<String>();
+	Scanner s = null;
+	String archive = "pacientes.txt";
+	File fichero = new File(archive);
+	String linea="";
+	try{
+            s = new Scanner(fichero);
+            while(s.hasNextLine()){
+                linea = s.nextLine();
+                String data[] = linea.split(",");
+                String infoPatient = data[2]+","+ data[1]+","+data[0];
+                dataArrayList.add(infoPatient);
+            }
 	}
+	catch(Exception ex){
+            System.out.println("Se tuvo el siguiente error: "+ex.getMessage());
+	}
+	finally{
+            try{
+		if(s!=null){
+                    s.close();
+                }
+            }
+            catch(Exception ex2){
+		System.out.println("Se tuvo el error: "+ ex2.getMessage());
+            }
+	}
+        System.out.println("Implementacion con JCF PriorityQueue:");
+        PriorityQueue<String> pQueue = new PriorityQueue<String>();
+        for(int i=0;i<dataArrayList.size();i++){
+            pQueue.add(dataArrayList.get(i));
+            i++;
+        }
+        System.out.println("La lista priorizada es la siguiente:");
+        while(!pQueue.isEmpty()){
+            System.out.println(pQueue.poll() + " ");
+        }
+    }
 }			
